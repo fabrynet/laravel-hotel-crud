6adbbc7e5e2a8c01e4b132bd3545cb42eb64dd1e
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-25 19:34:13 +0200

    Sistemazioni grafiche.

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -6,8 +6,14 @@
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
   </head>
   <body>
+    
     @include('partials.header')
-    @yield('content')
+
+    <main>
+      @yield('content')
+    </main>
+
     @include('partials.footer')
+
   </body>
 </html>
