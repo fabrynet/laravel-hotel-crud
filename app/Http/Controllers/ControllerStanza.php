fc2732504a4b5b13d453c1800a5e3a353fabfe4d
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-26 17:21:14 +0200

    Secondo metodo con save() per inserire una riga nel metodo store().

--- app/Http/Controllers/ControllerStanza.php
+++ app/Http/Controllers/ControllerStanza.php
@@ -19,8 +19,18 @@ class ControllerStanza extends Controller
       return view('stanze.create');
     }
     public function store(Request $request) {
-      $data = $request -> all(); // restituisce un array associativo con elementi _token, _method e i nomi dei valori passati dal form
-      $stanza = Stanza::create($data); // restituisce un oggetto con la riga inserita
+
+      //PRIMA VERSIONE
+      // $data = $request -> all(); // restituisce un array associativo con elementi _token, _method e i nomi dei valori passati dal form
+      // $stanza = Stanza::create($data); // restituisce un oggetto con la riga inserita
+
+      // SECONDA VERSIONE
+      $stanza = new Stanza();
+      $stanza -> room_number = $request -> room_number;
+      $stanza -> floor = $request -> floor;
+      $stanza -> beds = $request -> beds;
+      $stanza->save();
+
       return redirect() -> route('stanze-index');
     }
 }
